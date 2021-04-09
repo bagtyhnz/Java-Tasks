19c66acbecb6f04154df540c17954d7898813742
bagtyhnz <80709460+bagtyhnz@users.noreply.github.com>
2021-04-09 22:48:28 +0200

    Add files via upload

---
+++ task47.java
@@ -0,0 +1,19 @@
+package d11_NestedForDoWhileLoops;
+
+public class task47 {
+
+	public static void main(String[] args) {
+
+
+		for (int row = 0; row <7; row++) {
+			
+			for (int column = 0; column <=6; column++) {
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+		
+	}
+
+}
---
+++ task49.java
@@ -0,0 +1,20 @@
+package d11_NestedForDoWhileLoops;
+
+
+public class task49 {
+
+	public static void main(String[] args) {
+
+
+		for (int r = 0; r < 6; r++) {
+			System.out.println("#");
+			for (int c = 0; c <= r; c++) {
+				
+				
+				System.out.print(" ");;
+				
+			}
+		}
+	}
+
+}
