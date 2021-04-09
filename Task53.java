01ebf054ab981c04982f35db6f4acd9264687206
bagtyhnz <80709460+bagtyhnz@users.noreply.github.com>
2021-04-09 22:34:25 +0200

    Add files via upload

---
+++ Task53.java
@@ -0,0 +1,21 @@
+package d11_NestedForDoWhileLoops;
+
+public class Task53 {
+
+	public static void main(String[] args) {
+
+
+		int n=0, sum=0;
+		
+ 
+		while (n<100) {
+			
+			if (n%2==0) {
+			sum=sum+n;	
+			}
+			n++;
+		}			System.out.println(sum);
+
+	}
+
+}
